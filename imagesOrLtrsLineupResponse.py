566d7dcf3b62dba29ece2994281edb01a0c0dbad
Alex O. Holcombe <aoholcombe@gmail.com>
2017-08-16 12:27:57 +1000

    cleanup removing transpose

--- imagesOrLtrsLineupResponse.py
+++ imagesOrLtrsLineupResponse.py
@@ -28,7 +28,6 @@ def drawRespOption(myWin,bgColor,constantCoord,horizVert,color,drawBoundingBox,r
         x = constantCoord if horizVert else coord
         y = coord if horizVert else constantCoord
         if relativeSize != 1: #erase bounding box so erase old letter before drawing new differently-sized letter 
-            print('drawing to erase')
             boundingBox = visual.Rect(myWin,width=w,height=h, pos=(x,y), fillColor=bgColor, lineColor=None, units='norm' ,autoLog=False) 
             boundingBox.draw()
         option = visual.TextStim(myWin,colorSpace='rgb',color=color,alignHoriz='center', alignVert='center',
@@ -48,7 +47,6 @@ def drawRespOptionImage(myWin,bgColor,constantCoord,horizVert,color,drawBounding
         x = constantCoord if horizVert else coord
         y = coord if horizVert else constantCoord
         if relativeSize != 1: #erase bounding box so erase old letter before drawing new differently-sized letter 
-            print('drawing to erase')
             boundingBox = visual.Rect(myWin,width=w,height=h, pos=(x,y), fillColor=bgColor, lineColor=None, units='norm' ,autoLog=False) 
             boundingBox.draw()
         option = possibleResps[i]
@@ -226,7 +224,7 @@ def collectOneLineupResponseImage(myWin,bgColor,myMouse,drawBothSides,leftRightC
                             #print("whichResp from left hopefully = ",whichResp, " corresponding to ", possibleResps[whichResp])
                         #print("whichResp from top = ",whichResp, "xOffsetThis=",xOffsetThis, " About to redraw and draw one item in red")
                         lastValidClickButtons = deepcopy(pressed) #record which buttons pressed. Have to make copy, otherwise will change when pressd changes later
-                        print('lastValidClickButtons=',lastValidClickButtons)
+                        #print('lastValidClickButtons=',lastValidClickButtons)
                         state = 'waitingForClick' 
                 else: 
                     badClickSound.play()
@@ -248,7 +246,128 @@ def collectOneLineupResponseImage(myWin,bgColor,myMouse,drawBothSides,leftRightC
    
    #print('Returning with response=',response,'button=',button,' expStop=',expStop)
    return response, button, expStop
-        
+
+#def collectOneLineupResponse(myWin,imagesOrLetters,bgColor,myMouse,drawBothSides,leftRightCentral,OKtextStim,OKrespZone,possibleResps,xOffset,clickSound,badClickSound):
+#   if leftRightCentral == 0: #left
+#        constCoord = -1*xOffset
+#        horizVert = 1 #vertical
+#   elif leftRightCentral == 1: #right
+#        constCoord = xOffset
+#        horizVert = 1 #vertical
+#   elif leftRightCentral == 2: #central
+#        constCoord = 0
+#        OKrespZone.pos += [0,-.6]
+#        OKtextStim.pos+= [0,-.6]
+#        horizVert = 0 #horizontal
+#   
+#   myMouse.clickReset()
+#   sideIndicator = visual.Rect(myWin, width=.14, height=.04, fillColor=(1,1,1), fillColorSpace='rgb', lineColor=None, units='norm', autoLog=False)
+#   sideIndicatorCoord = .77*constCoord
+#   sideIndicator.setPos( [sideIndicatorCoord, 0] )
+#   chosenLtr = visual.TextStim(myWin,colorSpace='rgb',color=(1,1,1),alignHoriz='center', alignVert='center',height=.4,units='norm',autoLog=False)
+#   if horizVert: #vertical array
+#    chosenLtr.setPos( [sideIndicatorCoord,0] )  #big drawing of chosen letter, offset from lineup
+#   else: #horizontal array
+#    sideIndicatorCoord = -.3
+#    chosenLtr.setPos( [0,sideIndicatorCoord] )  #big drawing of chosen letter, offset from lineup
+#   
+#   whichResp = -1
+#   state = 'waitingForFirstClick' 
+#   #waitingForClick means OK is on the screen, so can either click a lineup item, or click OK
+#   #'finished' exit this lineup, choice has been made
+#   expStop = False
+#   while state != 'finished' and not expStop:
+#        #draw everything corresponding to this state
+#        drawResponseArrays(myWin,imagesOrLetters,bgColor,horizVert,xOffset,possibleResps,drawBothSides,leftRightCentral=leftRightCentral)
+#        if state == 'waitingForClick':
+#            #draw selected one in green, and bigly
+#            selectedColor = (-1,1,-1) #green
+#            buttonThis = np.where(pressed)[0] #assume only one button can be recorded as pressed
+#            if buttonThis == 0:
+#                selectedColor = (1,1,-1) #yellow for low confidence,
+#            if imagesOrLetters:
+#                drawRespOption(myWin,bgColor,constCoord,horizVert,selectedColor,False,1.5,possibleResps,whichResp)
+#            else: drawRespOptionImage(myWin,bgColor,constCoord,horizVert,selectedColor,False,1.5,possibleResps,whichResp)
+#            chosenLtr.setText(possibleResps[whichResp])
+#            chosenLtr.setColor( selectedColor )
+#            chosenLtr.draw()
+#            OKrespZone.draw()
+#            OKtextStim.draw()
+#        else:
+#            if leftRightCentral != 2:
+#                sideIndicator.draw()
+#            
+#        myWin.flip()
+#        #poll keyboard and mouse
+#
+#        #Used to use pressed,times = myMouse.getPressed(getTime=True) because it's supposed to return all presses since last call to clickReset. But, doesn't seem to work. So, now block
+#        #If getTime=True (False by default) then getPressed will return all buttons that have been pressed since the last call to mouse.clickReset as well as their time stamps:
+#        pressed,times = myMouse.getPressed(getTime=True)
+#        while not any(pressed): #wait until pressed
+#            pressed = myMouse.getPressed() 
+#        mousePos = myMouse.getPos()
+#        mousePos = convertXYtoNormUnits(mousePos,myWin.units,myWin)
+#        #Check what was clicked, if anything
+#        OK = False
+#        if any(pressed):
+#            if state == 'waitingForClick':
+#                OK = checkForOKclick(mousePos,OKrespZone)
+#                #print('OK=', OK)
+#                if OK:
+#                    state = 'finished'
+#            if not OK: #didn't click OK. Check whether clicked near response array item
+#                topmostCoord, topmostW, topmostH =  calcRespYandBoundingBox( possibleResps, horizVert, 0) #determine bounds of adjacent option
+#                topmostX = constCoord if horizVert else topmostCoord
+#                topmostY = topmostCoord if horizVert else constCoord
+#                btmmostCoord, btmmostW, btmmostH =  calcRespYandBoundingBox(possibleResps,horizVert, len(possibleResps)-1)
+#                btmmostX = constCoord if horizVert else btmmostCoord
+#                btmmostY = btmmostCoord if horizVert else constCoord
+#                w = topmostW
+#                h = topmostH
+#                if horizVert:
+#                    horizBounds = [ constCoord-w/2, constCoord+w/2 ]
+#                    vertBounds = [btmmostY - h/2, topmostY + h/2]
+#                else: #horizontal
+#                    horizBounds = [topmostX-w/2, btmmostX+w/2,]  #top letter in vertical is first in horizontal
+#                    vertBounds =  [constCoord-h/2, constCoord+w/2 ]
+#                #print("horizBounds=",horizBounds," vertBounds=",vertBounds, " constCoord=", constCoord)
+#                xValid = horizBounds[0] <= mousePos[0] <= horizBounds[1]  #clicked in a valid x-position
+#                yValid = vertBounds[0] <= mousePos[1] <= vertBounds[1]  #clicked in a valid y-position
+#                if xValid and yValid:
+#                        clickSound.play()
+#                        relToBtm = mousePos[1] - vertBounds[0] #mouse coordinates go up from -1 to +1
+#                        relToLeft = mousePos[0] - horizBounds[0]
+#                        if horizVert: #vertical
+#                            whichResp = int (relToBtm / h)
+#                            #change from relToBtm to relative to top
+#                            whichResp = len(possibleResps) - 1- whichResp 
+#                        else: #horizontal
+#                            whichResp = int(relToLeft / w)
+#                            #print("whichResp from left hopefully = ",whichResp, " corresponding to ", possibleResps[whichResp])
+#                        #print("whichResp from top = ",whichResp, "xOffsetThis=",xOffsetThis, " About to redraw and draw one item in red")
+#                        lastValidClickButtons = deepcopy(pressed) #record which buttons pressed. Have to make copy, otherwise will change when pressd changes later
+#                        print('lastValidClickButtons=',lastValidClickButtons)
+#                        state = 'waitingForClick' 
+#                else: 
+#                    badClickSound.play()
+#            for key in event.getKeys(): #only checking keyboard if mouse was clicked, hoping to improve performance
+#                key = key.upper()
+#                if key in ['ESCAPE']:
+#                    expStop = True
+#                    #noResponseYet = False
+#   response = possibleResps[whichResp]
+#   
+#   #Determine which button was pressed
+#   whichPressed = np.where(lastValidClickButtons)[0]
+#   if len(whichPressed)>1:
+#        print("Thought it was impossible to have pressed both buttons")
+#        print('whichPressed=',whichPressed)
+#   else:
+#        button = whichPressed[0]
+#   
+#   #print('Returning with response=',response,'button=',button,' expStop=',expStop)
+#   return response, button, expStop
+#   
 def doLineup(myWin,imagesOrLetters,bgColor,myMouse,clickSound,badClickSound,possibleResps,bothSides,leftRightCentral,autopilot):
     #leftRightCentral is 0 if draw on left side first (or only), 1 if draw right side first (or only), 2 if draw centrally only
     if type(leftRightCentral) is str: #convert to 0/1
@@ -341,10 +460,6 @@ def getImages(numImages):
             myWin.flip()
             
     imageObjects = [ imagesLeft , imagesRight ]
-    print('len(imageObjects)  = ',len(imageObjects), " len(imageObjects[0]) =", len(imageObjects[0]))
-    print("transpose")
-    imageObjectsT = zip(*imageObjects)
-    print('len(imageObjectsT)  = ',len(imageObjectsT), " len(imageObjectsT[0]) =", len(imageObjectsT[0]))
     return imageObjects
     
 if __name__=='__main__':  #Running this file directly, must want to test functions in this file
@@ -395,7 +510,7 @@ if __name__=='__main__':  #Running this file directly, must want to test functio
     expStop,passThisTrial,responses,buttons,responsesAutopilot = \
                 doLineup(myWin, imagesOrLetters, bgColor,myMouse, clickSound, badClickSound, possibleResps, bothSides, leftRightFirst, autopilot)
 
-    #print('autopilot=',autopilot, 'responses=',responses)
-    #print('expStop=',expStop,' passThisTrial=',passThisTrial,' responses=',responses, ' responsesAutopilot =', responsesAutopilot)
+    print('autopilot=',autopilot, 'responses=',responses)
+    print('expStop=',expStop,' passThisTrial=',passThisTrial,' responses=',responses, ' responsesAutopilot =', responsesAutopilot)
     
     print('Finished') 
